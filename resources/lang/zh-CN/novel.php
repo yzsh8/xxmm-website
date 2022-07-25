e7cd8a07744eaa83029533cc67508fda2e0b9c6c
james <yzsh8.james@gmail.com>
2022-07-25 13:46:27 +0800

    语言包

--- resources/lang/zh-CN/novel.php
+++ resources/lang/zh-CN/novel.php
@@ -11,7 +11,7 @@ return [
     'desc'      =>  '介绍',
     'ads'       =>  '小说右侧广告位',
     'asc'       =>  '正序',
-    'desc'      =>  '倒叙'
+    'desc'      =>  '倒叙',
     'catalogue' =>  '目录',
     'previous'  =>  '上一页',
     'next'      =>  '下一页',
